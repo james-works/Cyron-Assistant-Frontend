f402e31fd707dbf6c376a97980cf8197e2e5a1f6
AzzieRobel <wibipusahaf862@gmail.com>
2026-03-17 08:18:02 +0700

    Update dashboard

--- src/components/layout/Sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { GuildList } from '../GuildList';
-
-export const Sidebar = () => (
-  <aside className="hidden w-64 flex-shrink-0 border-r border-slate-200 bg-white/80 backdrop-blur-lg md:flex" style={{width: '20rem'}}>
-    <GuildList />
-  </aside>
-);
-
